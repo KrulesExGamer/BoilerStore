3be54c3cde205523343766afd0927aa2bde45d61
BernardoMC <88306132+bcmaia@users.noreply.github.com>
2023-06-07 00:08:57 -0300

    FrontEnd

---
+++ boilerstore-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import AppHeader from './component/AppHeader'
+
+const App = () => {
+  return (
+    <div className="App">
+      <AppHeader />
+    </div>
+  );
+};
+
+export default App;
