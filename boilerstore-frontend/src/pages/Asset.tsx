6afa8141fdbe09d0813283c9a7957758e7a453e5
BernardoMC <88306132+bcmaia@users.noreply.github.com>
2023-06-11 19:10:51 -0300

    Fake Fetch

--- boilerstore-frontend/src/pages/Asset.tsx
+++ boilerstore-frontend/src/pages/Asset.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import './Home.css'
+import './Asset.css'
 import { useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 
@@ -7,23 +7,54 @@ import EyeCatcher from '../shared/EyeCatcher';
 
 import { GameGenresSection, AssetTypesSection, ForYouSection, PopularThisWeekSection } from '../shared/MainSections'
 import Topics from '../shared/Topics';
-import {Slide} from '../components/ItemWindow'
+import { Slide } from '../components/ItemWindow'
 
-const AssetList = {
-    'AmazingCar': {title: 'aaa', slides : []}
+interface AssetData {
+    title: string, key: string, description: string, slides: Slide[]
+}
+
+const AssetList: { [key: string]: AssetData } = {
+    'AmazingCar': { title: 'AmazingCar', key: 'AmazingCar', description: 'An amizing car!', slides: [] }
 };
 
-const FetchAsset = async (assetKey : string) => {
-    return 
+const FetchAsset = async (FetchQuery: string) => {
+    const assetKey = FetchQuery.replace('/api/assets/', '');
+    return JSON.stringify(AssetList[assetKey]);
 };
 
 
 const Assets = () => {
     const location = useLocation();
-    const searchParams = new URLSearchParams(location.search);
+    const [assetData, setAssetData] = useState<AssetData | null>(null);
+
+    useEffect(() => {
+        const searchParams = new URLSearchParams(location.search);
+        const assetParam = searchParams.get('asset');
+
+        if (assetParam) {
+            FetchAsset(`/api/assets/${assetParam}`)
+                .then(response => JSON.parse(response))
+                .then(data => {
+                    // Handle the fetched data
+                    setAssetData(data);
+                })
+                .catch(error => {
+                    // Handle any errors
+                    console.error(error);
+                });
+        }
+    }, [location]);
 
     return (
-        <p>hello</p>
+        <section className='Asset'>
+            {null === assetData && (
+                <h2>Loading</h2>
+            )}
+
+            {null !== assetData && (
+                <h2>{assetData['title']}</h2>
+            )}
+        </section>
     );
 };
 
