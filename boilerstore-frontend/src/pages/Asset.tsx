f75291560589057ccf95e833cd9b3fe5d6baeba0
BernardoMC <88306132+bcmaia@users.noreply.github.com>
2023-06-11 20:33:27 -0300

    broken

--- boilerstore-frontend/src/pages/Asset.tsx
+++ boilerstore-frontend/src/pages/Asset.tsx
@@ -7,29 +7,45 @@ import EyeCatcher from '../shared/EyeCatcher';
 
 import { GameGenresSection, AssetTypesSection, ForYouSection, PopularThisWeekSection } from '../shared/MainSections'
 import Topics from '../shared/Topics';
-import { Slide } from '../components/ItemWindow'
+import { Slide, WindowData } from '../components/ItemWindow'
+import SectionSlider from '../components/SectionSlider';
 
 interface AssetData {
-    title: string, key: string, description: string, slides: Slide[]
+    status: string, title: string, key: string, description: string, widowData : WindowData
 }
 
+const assetNotFound: AssetData = { status: '404', title: '', key: '', description: '', slides: [] };
+
 const AssetList: { [key: string]: AssetData } = {
-    'AmazingCar': { title: 'AmazingCar', key: 'AmazingCar', description: 'An amizing car!', slides: [] }
+    'AmazingCar': { status: '200', title: 'AmazingCar', key: 'AmazingCar', description: 'An amizing car!', slides: [] }
 };
 
 const FetchAsset = async (FetchQuery: string) => {
     const assetKey = FetchQuery.replace('/api/assets/', '');
-    return JSON.stringify(AssetList[assetKey]);
+    return AssetList[assetKey] ? JSON.stringify(AssetList[assetKey]) : JSON.stringify(assetNotFound);
 };
 
 
+
 const Assets = () => {
+    const [imgIndex, setImgIndex] = useState(0);
+    const [assetKey, setAssetKey] = useState('');
+
     const location = useLocation();
     const [assetData, setAssetData] = useState<AssetData | null>(null);
+
+    const searchParams = new URLSearchParams(location.search);
+
 
     useEffect(() => {
         const searchParams = new URLSearchParams(location.search);
         const assetParam = searchParams.get('asset');
+        const imgIndexParam = searchParams.get('img_index');
+        
+        const intIndex = parseInt(null === imgIndexParam ? '0' : imgIndexParam);
+
+        setImgIndex(intIndex);
+        setAssetKey(null === assetParam ? '' : assetParam);
 
         if (assetParam) {
             FetchAsset(`/api/assets/${assetParam}`)
@@ -51,8 +67,21 @@ const Assets = () => {
                 <h2>Loading</h2>
             )}
 
-            {null !== assetData && (
-                <h2>{assetData['title']}</h2>
+            {null !== assetData && '200' === assetData['status'] && (
+                <div className='asset-container'>
+                    <h2>{assetData['title']}</h2>
+                    <div className='main-banner'>
+                        <img  />
+                    </div>
+                    <SectionSlider list={[]} colors={['rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0)']} itemQuery={`/asset?asset=${assetKey}&img_index=${imgIndex}`} searchQuery={''} />
+                </div>
+            )}
+
+            {null !== assetData && '404' === assetData['status'] && (
+                <div className='not-found'>
+                    <h2>Not Found</h2>
+                    <p> Ops! The asset was not found :\ </p>
+                </div>
             )}
         </section>
     );
