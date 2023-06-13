869d8557e203ffbb905c1654ca05fd7f99b6cfc4
KrulesExGamer <57680132+KrulesExGamer@users.noreply.github.com>
2023-06-13 13:37:41 -0300

    Recuperação está com UX melhorada

--- boilerstore-frontend/src/pages/Recovery.tsx
+++ boilerstore-frontend/src/pages/Recovery.tsx
@@ -1,51 +1,68 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { validateAccount } from '../utils/apiCalls';
 import '../shared_styles/alignment.css'
 import '../shared_styles/unselectable.css'
-import './Login.css'
-import { validateAccount } from '../utils/apiCalls';
+import './Recovery.css'
 
 const Recovery = () => {
-    const [email, setEmail] = useState("");
-
-    const [errorText, setError] = useState("");
-
-    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [errorText, setError] = useState('');
+    const [success, setSucess] = useState(false);
 
     const checkRecovery = () => {
-        if (email === "") {
-            setError("Erro: Email inválido!");
+        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+        
+        if (email === '' || !emailRegex.test(email)) {
+            setError('Erro: Email inválido!');
             return;
         }
 
+        else 
+            setError('');
 
-        if (validateAccount({task: "recovery", email: email}))
-            navigate("/login")
+
+        if (validateAccount({task: 'recovery', email: email}))
+            setSucess(true);
 
         else
-            setError("Erro: Email inválido!");
+            setError('Erro: Email não encontrado!');
     }
 
     return (
         <div className='conteiner-middle-center'><div className='item-middle-center'>
-            <div className='login-container'>
-                {errorText != "" && <p className='error'>{`${errorText}`}</p>}
+            <div className='recovery-container'>
+                {errorText !== '' && <p className='error'>{`${errorText}`}</p>}
                 
-                <p style={{fontWeight: "bolder", margin: "0% 10% 0% 10%", textAlign: "left"}}>
+                {
+                success === false &&
+                <>
+                <p className='recovery-message'>
                     Forgot your account’s password? Enter your email address and we’ll send you a recovery link.
                 </p>
                 <p>
-                    <input className='login_input'
-                        style={{marginRight: "20%"}}
+                    <input className='recovery_input'
+                        style={{marginRight: '20%'}}
                         onChange={(event)=>setEmail(event.target.value)} 
-                        type="email"
+                        type='email'
                         placeholder='Email...'
-                        id="email" />
+                        id='email' />
                 </p>
                 <p>
-                    <button className='login_button unselectable' onClick={checkRecovery}>Recovery</button>
+                    <button className='recovery_button unselectable' onClick={checkRecovery}>Recovery</button>
                 </p>
+                </>
+                }
+
+                {
+                success === true && (
+                <>
+                <p className='recovery-success'>Enviamos instruções de recuperação para o seu email.</p>
+                <Link to='/login'><p className='link'>Clique aqui para voltar para a página de Login</p></Link>
+                </>
+                )}
+
+
             </div>
         </div></div>
     )
