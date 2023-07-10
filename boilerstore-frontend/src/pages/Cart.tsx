d8a7370665ffd7cd068b1b24e273f6e69e9cbe51
BernardoMC <88306132+bcmaia@users.noreply.github.com>
2023-07-10 00:54:17 -0300

    Update Cart.tsx

--- boilerstore-frontend/src/pages/Cart.tsx
+++ boilerstore-frontend/src/pages/Cart.tsx
@@ -1,88 +1,98 @@
 import { Asset, CartItem } from '../utils/types';
-import "../img/zelda-like.png";
-import "../img/bullet-hell.jpeg" ;
-import './Cart.css'
+import '../img/zelda-like.png';
+import '../img/bullet-hell.jpeg';
+import './Cart.css';
 import { Link, useLocation } from 'react-router-dom';
 import { fetchCartAsset } from '../utils/apiCalls';
 import { useEffect, useState } from 'react';
 
-
-const cart : CartItem[] = [
-    {
-        assetId: '3d-red-ford-car',
-    },
-]
+const cart: CartItem[] = [
+	{
+		assetId: '3d-red-ford-car',
+	},
+];
 
 // Cart page layout
 const Cart = () => {
-    const location = useLocation();
+	const location = useLocation();
 
-    const [cartList, setCartList] = useState<Asset[]>([]);
-    const [totalPrice, setTotalPrice] = useState(0);
-    const [totalDiscount, setTotalDiscount] = useState(0);
+	const [cartList, setCartList] = useState<Asset[]>([]);
+	const [totalPrice, setTotalPrice] = useState(0);
+	const [totalDiscount, setTotalDiscount] = useState(0);
 
-    const updateCartPage = (tempList : Asset[]) => {
-        setCartList(tempList)
+	useEffect(() => {
+		const updateGameList = async () => {
+			let tempAssetList: Asset[] = [];
 
-        let t = 0;
-        cartList.forEach((game) => {
-            t += game.price;
-        }) 
+			for (const game of cart) {
+				const assetItem: Asset | undefined = await fetchCartAsset(
+					game.assetId,
+				);
+				if (
+					assetItem !== undefined &&
+					!tempAssetList.includes(assetItem)
+				)
+					tempAssetList.push(assetItem);
+			}
 
-        setTotalPrice(t);
-    }
+			setCartList(tempAssetList);
+		};
 
-    useEffect(() => {
-        const updateGameList = async () => { 
-            let tempList : Asset[] = [];
-            cart.forEach(async (game) => {
-                const assetItem : Asset | undefined = await fetchCartAsset(game.assetId);
-                if (assetItem !== undefined && !tempList.includes(assetItem))
-                    tempList.push(assetItem);
-            })
+		updateGameList();
+	}, [cart]);
 
-            updateCartPage(tempList)
-            
-            
+	useEffect(() => {
+		let t = 0;
+        
+		for (const game of cartList) {
+			t += game.price;
+		}
 
+		setTotalPrice(t);
 
-            console.log(cartList)
-        }
+		console.log(cartList);
+	}, [cartList]);
 
-        updateGameList();
+	const performPurchase = () => {};
 
-    }, [cart])
+	return (
+		<div className="cart">
+			{/* Por alguma razão, não consigo fazer os produtos aparecerem sem quebrar o resto do código */}
 
-    const performPurchase = () => {
-
-    }
-
-    return (
-        <div className='cart'>
-            {/* Por alguma razão, não consigo fazer os produtos aparecerem sem quebrar o resto do código */}
-            
-            <div className='buyBox'>
-                <div className="upper_buyBox">
-                    <div className="ubB_text">
-                        <p> Preço Total</p>
-                    </div>
-                    {(totalDiscount != 0) ?
-                        (<div className="pricing">
-                            <p className="totalPrice"> ${totalPrice.toFixed(2)}</p>
-                            <p className="totalDiscount"> ${totalDiscount.toFixed(2)}</p>
-                        </div>) :
-                        (<div className="pricing">
-                            <p className="totalDiscount"> ${totalPrice.toFixed(2)}</p>
-                        </div>)
-                    }
-                </div>
-                <div className="lower_buyBox">
-                    <Link to='/'><button className="back">Voltar a comprar</button></Link>
-                    <button className="buy">Comprar</button>
-                </div>
-            </div>
-        </div>
-    );
-}
+			<div className="buyBox">
+				<div className="upper_buyBox">
+					<div className="ubB_text">
+						<p> Preço Total</p>
+					</div>
+					{totalDiscount != 0 ? (
+						<div className="pricing">
+							<p className="totalPrice">
+								{' '}
+								${totalPrice.toFixed(2)}
+							</p>
+							<p className="totalDiscount">
+								{' '}
+								${totalDiscount.toFixed(2)}
+							</p>
+						</div>
+					) : (
+						<div className="pricing">
+							<p className="totalDiscount">
+								{' '}
+								${totalPrice.toFixed(2)}
+							</p>
+						</div>
+					)}
+				</div>
+				<div className="lower_buyBox">
+					<Link to="/">
+						<button className="back">Voltar a comprar</button>
+					</Link>
+					<button className="buy">Comprar</button>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Cart;
